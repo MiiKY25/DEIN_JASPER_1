318038b1e52367f61eedcd3e3b2268cc715610d8
MiiKY <mikel.ra.2003@gmail.com>
2025-01-06 21:07:59 +0100

    add:Si la conexion con la bbdd es fallida salta un error

--- src/main/java/org/mikel/dein_jasper_1/LanzadorJasper.java
+++ src/main/java/org/mikel/dein_jasper_1/LanzadorJasper.java
@@ -1,5 +1,9 @@
 package org.mikel.dein_jasper_1;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
@@ -13,14 +17,29 @@ import java.util.Map;
 /**
  * Clase principal para ejecutar un informe JasperReports.
  */
-public class LanzadorJasper {
+public class LanzadorJasper extends Application {
+
 
     /**
-     * Metodo principal que lanza el informe JasperReports.
+     * Metodo principal de ejecución de la aplicación. Llama al metodo {@link #start(Stage)}
+     * para inicializar y mostrar el informe basado en JasperReports.
      *
-     * @param args los argumentos de línea de comandos
+     * @param args Argumentos de línea de comandos.
      */
     public static void main(String[] args) {
+        launch(args); // Inicia la aplicación JavaFX
+    }
+
+
+    /**
+     * Metodo encargado de la lógica principal para generar y mostrar el informe.
+     * Este metodo establece la conexión a la base de datos, carga el informe JasperReport,
+     * rellena el informe con datos de la base de datos y finalmente lo muestra usando un visor de informes.
+     *
+     * @param primaryStage El escenario principal de la aplicación.
+     */
+    @Override
+    public void start(Stage primaryStage) {
         ConexionBBDD db;
         try {
             db = new ConexionBBDD();
@@ -45,9 +64,27 @@ public class LanzadorJasper {
             viewer.setVisible(true);
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            mostrarError("No se ha podido establecer conexion con la Base de Datos");
+            Platform.exit();
         } catch (JRException e) {
             e.printStackTrace();
+            mostrarError("Error al generar el informe");
+            Platform.exit();
+        }finally {
+            Platform.exit();
         }
+    }
+
+    /**
+     * Metodo que muestra un mensaje de error en una ventana emergente.
+     *
+     * @param error El mensaje de error que se mostrará en la ventana emergente.
+     */
+    void mostrarError(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(error);
+        alert.showAndWait();
     }
 }
